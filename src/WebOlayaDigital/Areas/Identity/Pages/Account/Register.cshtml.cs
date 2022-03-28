662981c98bb73e46524056b8c9379ec6505c47f4
JhotaMS <Jhotams.developer@gmail.com>
2022-03-28 02:25:29 -0500

    Configuración de SENDGRIF

--- src/WebOlayaDigital/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ src/WebOlayaDigital/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -11,6 +11,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using WebOlayaDigital.Interfaces;
 using WebOlayaDigital.Models;
@@ -114,8 +115,8 @@ namespace WebOlayaDigital.Areas.Identity.Pages.Account
                             values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
                             protocol: Request.Scheme);
 
-                        //await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        //    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                        await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                            $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
 
                         //Realizar la inserción de la base de dato de la base datos.
--- src/WebOlayaDigital/Services/Commons/EmailSender.cs
+++ src/WebOlayaDigital/Services/Commons/EmailSender.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Configuration;
 using SendGrid;
 using SendGrid.Helpers.Mail;
+using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -9,27 +10,29 @@ namespace WebOlayaDigital.Services.Commons
 {
     public class EmailSender : IEmailSender
     {
-        private readonly IConfiguration _configuration;
         private static readonly HttpClient HttpClient = new HttpClient();
-
-        public EmailSender(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
 
         public async Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            string apiKey = _configuration.GetValue<string>("ConfigUrls:SendGrid");
+            string apiKey = Environment.GetEnvironmentVariable("SendGrid");
 
             var client = new SendGridClient(HttpClient, new SendGridClientOptions { ApiKey = apiKey, HttpErrorAsException = true });
 
             // Send a Single Email using the Mail Helper
-            var from = new EmailAddress("alexa.a.d.a1998@gmail.com");
-            var to = new EmailAddress(email, "Example User");
-            var plainTextContent = subject;
-            var htmlContent = htmlMessage;
-            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
-            _ = await client.SendEmailAsync(msg);
+            try
+            {
+                var from = new EmailAddress("alexa.a.d.a1998@gmail.com");
+                var to = new EmailAddress(email, "Example User");
+                var plainTextContent = subject;
+                var htmlContent = htmlMessage;
+                var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+                _ = await client.SendEmailAsync(msg);
+            }
+            catch (Exception)
+            {
+                
+            }
+
 
         }
     }
